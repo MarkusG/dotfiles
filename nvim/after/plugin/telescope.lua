751d21ad72b8e61f9227ec11f020622dce53d50c
Mark Gross <mark.gross2001@gmail.com>
2023-04-04 15:18:23 -0700

    Telescope mappings

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -4,3 +4,15 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+
+require('telescope').setup {
+    defaults = {
+        mappings = {
+            i = {
+                ['<CR>'] = 'select_tab',
+                -- for in-place
+                ['<C-i>'] = 'select_default'
+            }
+        }
+    }
+}
