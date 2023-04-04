76882b5a1025f9737779aa1cf0cb235a4628300b
Mark Gross <mark.gross2001@gmail.com>
2023-04-04 15:10:01 -0700

    More color improvements

--- nvim/after/plugin/base16.lua
+++ nvim/after/plugin/base16.lua
@@ -1,10 +1,18 @@
 vim.cmd.colorscheme('base16-default-dark')
+
 vim.cmd.highlight({ 'TabLine', 'ctermfg=White ctermbg=Black' })
 vim.cmd.highlight({ 'TabLineFill', 'ctermfg=White ctermbg=Black' })
 vim.cmd.highlight({ 'TabLineSel', 'ctermfg=Black ctermbg=White' })
 vim.cmd.highlight({ 'StatusLine', 'ctermfg=White ctermbg=Black' })
+vim.cmd.highlight({ 'StatusLineNc', 'ctermfg=White ctermbg=Black' })
 vim.cmd.highlight({ 'ColorColumn', 'ctermbg=DarkGray' })
 vim.cmd.highlight({ 'LineNrAbove', 'ctermfg=DarkGray' })
 vim.cmd.highlight({ 'LineNr', 'ctermfg=White' })
 vim.cmd.highlight({ 'LineNrBelow', 'ctermfg=DarkGray' })
+
 vim.cmd.highlight({ 'Comment', 'ctermfg=Gray' })
+vim.cmd.highlight({ 'Visual', 'ctermbg=DarkGray' })
+
+vim.cmd.highlight({ 'NormalFloat', 'ctermfg=White ctermbg=DarkGray' })
+vim.cmd.highlight({ 'Pmenu', 'ctermfg=White ctermbg=DarkGray' })
+vim.cmd.highlight({ 'PmenuSel', 'ctermfg=DarkGray ctermbg=White' })
