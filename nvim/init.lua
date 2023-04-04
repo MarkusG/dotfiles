47f44ee74ba2732da748f59b9dbd1ebf9d7870f7
Mark Gross <mark.gross2001@gmail.com>
2023-04-04 15:21:34 -0700

    Create a reason to thank myself

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,8 @@
+-- here are some instructions on how to make this work on a new machine for when
+-- you inevitably forget:
+--
+-- 1. cd .config; ln -s ~/dotfiles/nvim
+-- 2. install packer (just go to its GitHub and run the snippet)
+-- 3. open packer.lua -> :so -> :PackerSync
+
 require('mark')
