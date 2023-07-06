fe0a27f4ead7a77d8de257f4d7cff1ee1ea1b4a8
Mark Gross <mark.gross2001@gmail.com>
2023-07-06 11:05:03 -0700

    Change ex keybind

--- nvim/lua/mark/remap.lua
+++ nvim/lua/mark/remap.lua
@@ -1,3 +1,3 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<C-e>", vim.cmd.Ex)
 vim.keymap.set("n", "H", "gT", { remap = true })
 vim.keymap.set("n", "L", "gt", { remap = true })
