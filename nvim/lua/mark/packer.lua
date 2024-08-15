5dda76e0856cf2e2b08e7a4cf12824345fa1f691
Mark Gross <mark.gross2001@gmail.com>
2024-08-14 21:30:52 -0700

    Update nvim-telescope

--- nvim/lua/mark/packer.lua
+++ nvim/lua/mark/packer.lua
@@ -17,7 +17,7 @@ return require('packer').startup(function(use)
     })
 
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        'nvim-telescope/telescope.nvim', tag = '0.1.8',
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
 
